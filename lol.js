77fe2ca249190a0f99ec895c56d80573ea2f0cdc
Elliot <59733209+ElliotDoesCode@users.noreply.github.com>
2021-11-20 11:54:04 -0500

    Update lol.js

--- lol.js
+++ lol.js
@@ -2,10 +2,22 @@ if (document.title.indexOf("Grades and Attendance") != -1) {
     function getSecondPart(str) {
         return str.split('_')[1];
     }
-    
-    function ins(txt, pat) {
-        var newElement = document.createElement("div");
+
+
+    function ins(txxt, link) {
+        txt = txxt+" \n"
+        var newElement;
+        if (link && link.includes("keep")) {
+            newElement = document.createElement("a");
+        } else {
+            newElement = document.createElement("div")
+        }
         newElement.appendChild(document.createTextNode(txt));
+        if (link && link.includes("keep")) {
+            newElement.title = txt
+            newElement.href = link
+            newElement.target = "_blank"
+        }
         document.getElementById('attByClass').appendChild(newElement);
     }
     
@@ -13,6 +25,71 @@ if (document.title.indexOf("Grades and Attendance") != -1) {
         if (typeof str != "string") return false  
         return !isNaN(str) &&
                !isNaN(parseFloat(str))
+    }
+
+    function getRanking(ok,lol) {
+        //now get your grade :)
+        var name = document.querySelector("#userName > span").innerText.split(/(\s+)/)[0];
+        var users = lol.players
+        var founduser;
+        var foundscore;
+        for (var item in users) {
+            if (users[item].player_name && users[item].player_name == name) {
+                founduser = users[item];
+                foundscore = parseInt(item) + 1;
+                break;
+            }
+        }
+
+        //first request start
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({"player_id":founduser.id,"score":-founduser.score});
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch("https://keepthescore.co/api/yjxrblmwdae/score/?=&=", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+        //first request end
+
+        //second request start
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        var raw = JSON.stringify({"player_id":founduser.id,"score":ok});
+
+        var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+        };
+
+        fetch("https://keepthescore.co/api/yjxrblmwdae/score/?=&=", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
+        //second request end
+
+        console.log("a: "+name, " b: "+founduser.player_name)
+        return foundscore
+    }
+    async function getBestrank(ok, callbac) {
+        var aa = "https://keepthescore.co/api/yjxrblmwdae/board/";
+        var obj = await (await fetch(aa)).json();
+        ouput = (obj.players[0].player_name)
+        console.log(ouput)
+        ins(ouput+" has the best grade")
+        ins("You have the "+getRanking(ok,obj)+" best grade in the class","https://keepthescore.co/board/yjxrblmwdae/")
+        callbac()
     }
     
     function getAverage(ss) {
@@ -126,12 +203,14 @@ if (document.title.indexOf("Grades and Attendance") != -1) {
     getAverage();
     ins("|------------------|")
     ins("GPA - "+gpa(parseInt(getAverage("yeah"))));
-    
-    //credits
-    ins("___________________");
-    ins("Credits to Elliot. Version 0.11")
-    //ew
-    replaceWithLetter();
+    ins("|------------------|")
+    getBestrank(parseInt(getAverage("yeah")), function() {
+        //credits
+        ins("___________________");
+        ins("Elliot's amazing #baddie. Version 0.11")
+        //ew
+        replaceWithLetter();
+    })
 } else if (document.title.indexOf("Class Score Detail") != -1) {
     //If you clicked on a grade yk
 }
