8faedc8b0b09d0e2f8b865f8241f1a91d3642091
Elliot <59733209+ElliotDoesCode@users.noreply.github.com>
2022-01-04 18:39:35 -0800

    Update lol.js

--- lol.js
+++ lol.js
@@ -120,7 +120,7 @@ if (document.title.indexOf("Grades and Attendance") != -1) {
         ouput = (obj.players[0].player_name)
         console.log(ouput)
         ins(ouput+" has the best grade")
-        ins("You have the #"+getRanking(ok,obj)+" best grade in the class","https://elliotdoescode.github.io/POWERschool/rawr.html")
+        ins("You have the #"+getRanking(ok,obj)+" best grade in the class","https://elliotdoescode.github.io/test/")
         callbac()
     }
     
